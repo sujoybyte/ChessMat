a9c934acdefdafc985c02728066140960842288d
Sujoy Ghosh <sujoybyte@gmail.com>
2020-08-24 06:38:14 +0530

    added script to fit board across screen sizes

---
+++ Assets/Scripts/BoardFit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BoardFit : MonoBehaviour
+{
+    public float boardSpace = 4.3f;
+
+    private void Update()
+    {
+        Camera.main.orthographicSize = boardSpace * Screen.height / Screen.width;
+    }
+}
